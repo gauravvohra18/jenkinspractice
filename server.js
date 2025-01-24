17565bf7c147c5ff77f09d2f5eb276ce0f88a5f9
gauravvohra18 <gauravvohradevops@gmail.com>
2025-01-24 18:00:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world I am samarth and gaurav');
+  res.send('Hello world I am gautam');
 });
 
 app.listen(PORT, HOST);
